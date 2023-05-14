c97f19d72092e806e388ca0f1909bc8522477f93
Dagogo Orifama <dagoris2010@gmail.com>
2023-05-14 18:40:52 +0000

    Uploaded files

---
+++ models/state.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+    The State Module(inherits from the BaseModel)
+"""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """
+        it implements state model
+    """
+    name = ''
